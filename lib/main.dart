84a89e65c566681af4e216571b671974ac882ee5
Elves Aguiar <elvesmhs@hotmail.com>
2023-12-13 16:26:47 -0300

    feat: questao component

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './questao.dart';
 
 main() => runApp(PerguntaApp());
 final List<String> perguntas = [
@@ -24,7 +25,7 @@ class _PerguntaAppState extends State<PerguntaApp> {
             title: Text('perguntas'),
           ),
           body: Column(children: [
-            Text(perguntas.elementAt(_perguntaSelecionada)),
+            Questao(perguntas.elementAt(_perguntaSelecionada)),
             RaisedButton(child: Text('Resposta 1'), onPressed: _responder),
             RaisedButton(child: Text('Resposta 2'), onPressed: _responder),
             RaisedButton(child: Text('Resposta 3'), onPressed: _responder)
