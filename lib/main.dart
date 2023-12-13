4531ff400d11704e2d4c0c06acec209e16e4d4f0
Elves Aguiar <elvesmhs@hotmail.com>
2023-12-13 12:03:06 -0300

    feat: funções como parametro no futtler

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,32 @@
-
-
 import 'package:flutter/material.dart';
 
-main() =>  runApp(new testeApp());
+main() => runApp(new testeApp());
 
-
-class testeApp extends StatelessWidget{
+class testeApp extends StatelessWidget {
+  final List<String> perguntas = [
+    'Qual a sua cor favorita?',
+    'Qual o seu animal favorito?'
+  ];
+  void responder () {
+    print('pergunta resposndida!!!');
+  }
   @override
-  Widget build( BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('perguntas'),
-          
-        ),
-        body: Text('Ola flutter'),
-      ),
+          appBar: AppBar(
+            title: Text('perguntas'),
+          ),
+          body: Column(children: [
+            Text(perguntas.elementAt(0)),
+            RaisedButton(child: Text('Resposta 1'), onPressed: responder),
+            RaisedButton(child: Text('Resposta 2'), onPressed: () {
+              print('respindeu 2');
+            }),
+            RaisedButton(child: Text('Resposta 3'), onPressed: () => {
+              print('pressionou 3')
+            })
+          ])),
     );
   }
 }
