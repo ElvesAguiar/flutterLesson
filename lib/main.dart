965e49a4d29729b78f269dd13f69370591dfcc95
Elves Aguiar <elvesmhs@hotmail.com>
2023-12-14 16:26:32 -0300

    feat: adding score

--- lib/main.dart
+++ lib/main.dart
@@ -7,42 +7,45 @@ void main() => runApp(const PerguntaApp());
 
 class _PerguntaAppState extends State<PerguntaApp> {
   var _perguntaSelecionada = 0;
+  var _pontuacaototal = 0;
   final _perguntas = const [
     {
       'texto': 'Qual é a sua cor favorita?',
      'respostas': [
-        {'texto': 'Preto', 'nota': 10},
-        {'texto': 'Vermelho', 'nota': 6},
-        {'texto': 'Verde', 'nota': 8},
-        {'texto': 'Branco', 'nota': 5},
+        {'texto': 'Preto', 'pontuacao': 10},
+        {'texto': 'Vermelho', 'pontuacao': 6},
+        {'texto': 'Verde', 'pontuacao': 8},
+        {'texto': 'Branco', 'pontuacao': 5},
       ],
     },
     {
       'texto': 'Qual é o seu animal favorito?',
       'respostas': [
-        {'texto': 'Coelho', 'nota': 10},
-        {'texto': 'Cobra', 'nota': 5},
-        {'texto': 'Elefante', 'nota': 3},
-        {'texto': 'Leão', 'nota': 1},
+        {'texto': 'Coelho', 'pontuacao': 10},
+        {'texto': 'Cobra', 'pontuacao': 5},
+        {'texto': 'Elefante', 'pontuacao': 3},
+        {'texto': 'Leão', 'pontuacao': 1},
       ],
     },
     {
       'texto': 'Qual é o seu instrutor favorito?',
       'respostas': [
-        {'texto': 'Maria', 'nota': 10},
-        {'texto': 'João', 'nota': 8},
-        {'texto': 'Leo', 'nota': 4},
-        {'texto': 'Pedro', 'nota': 7},
+        {'texto': 'Maria', 'pontuacao': 10},
+        {'texto': 'João', 'pontuacao': 8},
+        {'texto': 'Leo', 'pontuacao': 4},
+        {'texto': 'Pedro', 'pontuacao': 7},
       ],
     },
   ];
 
-  void _responder() {
+  void _responder(int pontuacao) {
     if (temPerguntaSelecionada) {
       setState(() {
         _perguntaSelecionada++;
+        _pontuacaototal+=pontuacao;
       });
     }
+    print(_pontuacaototal);
   }
 
   bool get temPerguntaSelecionada {
--- lib/questionario.dart
+++ lib/questionario.dart
@@ -5,7 +5,7 @@ import './resposta.dart';
 class Questionario extends StatelessWidget {
   final List<Map<String, Object>> perguntas;
   final int perguntaSelecionada;
-  final void Function() quandoResponder;
+  final void Function(int) quandoResponder;
 
   const Questionario({
     required this.perguntas,
@@ -28,9 +28,12 @@ class Questionario extends StatelessWidget {
     return Column(
       children: [
         Questao(perguntas[perguntaSelecionada]['texto'] as String),
-        ...respostas
-            .map((resp) => Resposta(resp['texto'] as String, quandoResponder))
-            .toList(),
+        ...respostas.map((resp) {
+          return Resposta(
+            resp['texto'].toString(),
+            () => quandoResponder(int.parse(resp['pontuacao'].toString())),
+          );
+        })
       ],
     );
   }
